a9f1ab237efb3236f11c23a5eb527964d5e49f28
Andreea <andreea_popa22@yahoo.ro>
2023-01-05 15:36:53 +0200

    initialize project

---
+++ code/SmartAgenda/src/main/java/com/example/smartagenda/SmartAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAgendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAgendaApplication.class, args);
+    }
+
+}
