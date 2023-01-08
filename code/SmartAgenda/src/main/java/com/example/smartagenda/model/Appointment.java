7c61ed3c1ee85535c51cb827b67db95a1fd0ae0a
Andreea <andreea_popa22@yahoo.ro>
2023-01-09 00:14:00 +0200

    code first working

--- code/SmartAgenda/src/main/java/com/example/smartagenda/model/Appointment.java
+++ code/SmartAgenda/src/main/java/com/example/smartagenda/model/Appointment.java
@@ -1,15 +1,28 @@
 package com.example.smartagenda.model;
 
-import com.example.smartagenda.helper.Hour;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalTime;
 import java.util.Date;
 
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "appointments")
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int appointmentId;
     private int clientId;
     private int providerId;
     private int locationId;
     private Date date;
-    private Hour startTime;
-    private Hour endTime;
+    private LocalTime startTime;
+    private LocalTime endTime;
 }
